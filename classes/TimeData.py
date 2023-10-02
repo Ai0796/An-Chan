694d26717f125b45dc19199a30ac0edc1211ae31
Ai0796 <mo.alden25@gmail.com>
2023-10-02 21:10:38 +0000

    made add also combine checkins

--- classes/TimeData.py
+++ classes/TimeData.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class TimeData:
     """
     Contains information for a single timestamp
@@ -23,30 +22,4 @@ class TimeData:
     
     def add(self, timeData):
         self.orders.extend(timeData.orders)
-=======
-class TimeData:
-    """
-    Contains information for a single timestamp
-    """
-    
-    def __init__(self, timestamp, players, checkIns):
-        self.timestamp = timestamp
-        self.orders = players
-        self.checkIns = checkIns
-        
-    def addPlayer(self, player):
-        self.orders.append(player)
-        
-    def addCheckIn(self, player):
-        self.checkIns.append(player)
-        
-    def getOrders(self):
-        return self.orders
-        
-    def getCheckIns(self):
-        return self.checkIns
-    
-    def add(self, timeData):
-        self.orders.extend(timeData.orders)
->>>>>>> 918c16b14ac8f67325046363b7a9369592853ac9
         self.checkIns.extend(timeData.checkIns)
