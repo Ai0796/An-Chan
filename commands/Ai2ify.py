122381536bd1d5a85e896d1d1c4055b335bfc76f
Ai0796 <mo.alden25@gmail.com>
2024-01-19 01:30:18 +0000

    changed discord nickname of Ai2

--- commands/Ai2ify.py
+++ commands/Ai2ify.py
@@ -12,8 +12,8 @@ class Ai2ify(commands.Cog):
 
         self.bot.config.setRequestType(guild.id, 'Ai2')
 
-        await guild.me.edit(nick='Ai2.0-chan')
-        await ctx.respond('The bot is now Ai2 in this server')
+        await guild.me.edit(nick='杏ちゃん')
+        await ctx.respond('The bot is now An2 in this server')
 
 
 def setup(bot):
